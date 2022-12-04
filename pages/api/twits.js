eb55a440c8196365df93185a0818a48e002ad344
Juan Jacobo <juizquierdob@unal.edu.co>
2022-12-04 18:24:39 -0500

    testing api

--- pages/api/twits.js
+++ pages/api/twits.js
@@ -5,11 +5,16 @@ import {GetCommand} from "@aws-sdk/lib-dynamodb";
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         const {Item} = await mongoDBClient.send(
-            new GetCommand({
+            new QueryCommand({
+                    KeyConditionExpression: "Season = :s and Episode > :e",
+                    FilterExpression: "contains (Subtitle, :topic)",
+                    ExpressionAttributeValues: {
+                        ":s": {N: "1"},
+                        ":e": {N: "2"},
+                        ":topic": {S: "SubTitle"},
+                    },
+                    ProjectionExpression: "Episode, Title, Subtitle",
                     TableName: "mirandaz",
-                    Key: {
-                        user: "echelecabeza",
-                    }
                 }
             ));
 
